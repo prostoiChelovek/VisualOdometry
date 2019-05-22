0312a1c5fd75c65af68597aba59607f4c65e7a48
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2019-05-22 23:33:04 +0500

    beautify a little

--- main.cpp
+++ main.cpp
@@ -1,42 +1,38 @@
-
 #include "vo.h"
+
 using namespace std;
 
-int main(int argc, char** argv) {
+string datasetPath = "/home/prostoichelovek/Documents/datasets/dataset";
 
+int main(int argc, char **argv) {
+    int max_frame = 600;
+    if (argc > 1)
+        max_frame = atoi(argv[1]);
 
-	if (argc != 2)
-    {
-        cout<<"Usage: ba max_frame"<<endl;
-        exit(1);
-    }
+    //you have to configure your own path
+    string left_path = datasetPath + "/sequences/00/image_0/%06d.png";
+    string right_path = datasetPath + "/sequences/00/image_1/%06d.png";
+    string pose_path = datasetPath + "/poses/00.txt";
 
-    int max_frame= atoi(argv[1]);
+    cout << "Program starts!" << endl;
 
-	//you have to configure your own path
-	string left_path =  "/Users/HJK-BD/Downloads/kitti/00/image_0/%06d.png";
-	string right_path = "/Users/HJK-BD/Downloads/kitti/00/image_1/%06d.png";
-	string pose_path = "/Users/HJK-BD/Downloads/kitti/poses/00.txt";
+    cv::Mat K = (cv::Mat_<double>(3, 3) << 7.188560000000e+02, 0, 6.071928000000e+02,
+            0, 7.188560000000e+02, 1.852157000000e+02,
+            0, 0, 1);
 
-	cout <<"Program starts!"<<endl;
+    float baseline = 0.54;
 
-	cv::Mat K = (cv::Mat_<double>(3, 3) << 7.188560000000e+02, 0, 6.071928000000e+02,
-                         0, 7.188560000000e+02, 1.852157000000e+02,
-                         0, 0, 1);
+    VO vo(K, baseline, left_path, right_path);
 
-	float baseline = 0.54;
+    //we have only 1000 paris of pictures
+    vo.set_Max_frame(max_frame);
 
-	VO vo(K, baseline, left_path, right_path);
+    //This is to get the ground truethe pose data
+    vector<vector<float>> poses = VO::get_Pose(pose_path);
 
-	//we have only 1000 paris of pictures
-	vo.set_Max_frame(max_frame);
+    vo.playSequence(poses);
 
-	//This is to get the ground truethe pose data
-	vector<vector<float>> poses = VO::get_Pose(pose_path);
+    cout << "You come to the end!" << endl;
 
-	vo.playSequence(poses);
-
-	cout << "You come to the end!" << endl;
-
-	return 0;
+    return 0;
 }
--- vo.h
+++ vo.h
@@ -1,7 +1,5 @@
-
 #ifndef VO_H
 #define VO_H
-
 
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/core/core.hpp"
@@ -9,45 +7,53 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/features2d.hpp"
 
-
 class VO {
-
 public:
+    VO() = default;
 
-	VO(){}
-	virtual ~VO(){}
-	VO(const cv::Mat& _k, float _baseline, const std::string&, const std::string&);
+    virtual ~VO() = default;
 
-	//input is the feature points 1 and feature points 2 in each image, output is the 3D points, stereo images
-	std::vector<cv::Point3f> get3D_Points(const std::vector<cv::Point2f>& feature_p1, const std::vector<cv::Point2f>& feature_p2) const ;
+    VO(const cv::Mat &_k, float _baseline, const std::string &, const std::string &);
 
-	//input  is two stere image pair, output is the feature points of image1 and generated 3D points of landmarks
-	void extract_keypoints_surf(const cv::Mat& img1, const cv::Mat& img2, std::vector<cv::Point3f>&landmarks, std::vector<cv::Point2f>& feature_points) const ;
+    //input is the feature points 1 and feature points 2 in each image, output is the 3D points, stereo images
+    std::vector<cv::Point3f> get3D_Points(const std::vector<cv::Point2f> &feature_p1,
+                                          const std::vector<cv::Point2f> &feature_p2) const;
 
-	//input start, inv_transform is the transformation matrix,  output is featurePoints-2D points of features, landmarks is 3D points of landmarkds
-	void create_new_features(int start, const cv::Mat& inv_transform,  std::vector<cv::Point2f>& featurePoints, std::vector<cv::Point3f>& landmarks) const ;
+    //input  is two stere image pair, output is the feature points of image1 and generated 3D points of landmarks
+    void extract_keypoints_surf(const cv::Mat &img1, const cv::Mat &img2,
+                                std::vector<cv::Point3f> &landmarks,
+                                std::vector<cv::Point2f> &feature_points) const;
 
-	//this is to play on the image sequence
-	void playSequence(const std::vector<std::vector<float>>& poses) const ;
+    //input start, inv_transform is the transformation matrix,  output is featurePoints-2D points of features, landmarks is 3D points of landmarkds
+    void create_new_features(int start, const cv::Mat &inv_transform,
+                             std::vector<cv::Point2f> &featurePoints,
+                             std::vector<cv::Point3f> &landmarks) const;
 
-	cv::Mat getK() const {return K;}
-	float getBaseline() const {return baseline;}
-	void setK(const cv::Mat& _K) {K = _K;}
-	void setBaseline(float _baseline) {baseline = _baseline;}
-	void set_Max_frame(int maxframe) {max_frame = maxframe;}
+    //this is to play on the image sequence
+    void playSequence(const std::vector<std::vector<float>> &poses) const;
 
-	//get the true pose files and convert it into vector form
-	static std::vector<std::vector<float>> get_Pose(const std::string& path);
-	//get the left image
-	static cv::Mat getImage(const std::string& path, int i);
+    cv::Mat getK() const { return K; }
+
+    void setK(const cv::Mat &_K) { K = _K; }
+
+    float getBaseline() const { return baseline; }
+
+    void setBaseline(float _baseline) { baseline = _baseline; }
+
+    void set_Max_frame(int maxframe) { max_frame = maxframe; }
+
+    //get the true pose files and convert it into vector form
+    static std::vector<std::vector<float>> get_Pose(const std::string &path);
+
+    //get the left image
+    static cv::Mat getImage(const std::string &path, int i);
 
 private:
-	cv::Mat K;
-	float baseline;
-	int max_frame;
-	std::string left_image_path;
-	std::string right_image_path;
+    cv::Mat K;
+    float baseline;
+    int max_frame;
+    std::string left_image_path;
+    std::string right_image_path;
 };
-
 
 #endif
